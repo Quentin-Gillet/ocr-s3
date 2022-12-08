7a92c605a2c60cb3c76b72afd4a8fb9073b2dc45
Alexis COGNET <alexis.cognet@epita.fr>
2022-12-08 20:14:40 +0100

    Image is now resized when automatically when choosen

--- interfacegtk.c
+++ interfacegtk.c
@@ -10,11 +10,10 @@ void create_image(GtkFileChooserButton* file_chooser_button, gpointer user_data)
     GFile* file = gtk_file_chooser_get_file(GTK_FILE_CHOOSER  (file_chooser_button) );
     const char * filename = g_file_get_path(file); 
     printf("filename : %s",filename);
-    gtk_image_set_from_file(info->image,filename); // filename est un const char*
+    const GdkPixbuf * pixbuf = gdk_pixbuf_new_from_file(filename,NULL);
+    GdkPixbuf * ResizedPixbuf = gdk_pixbuf_scale_simple(pixbuf,1000,850,GDK_INTERP_BILINEAR);
+    gtk_image_set_from_pixbuf(info->image,ResizedPixbuf); // filename est un const char*
     
-    
-    // chargement de l'image
-    //info->image = xxx;
 }
 
 // Main function.
@@ -40,14 +39,16 @@ int main (int argc, char *argv[])
     GtkWindow* window = GTK_WINDOW(gtk_builder_get_object(builder, "window")); 
     GtkFileChooserButton* file_chooser = GTK_FILE_CHOOSER_BUTTON(gtk_builder_get_object(builder, "file_chooser"));
     GtkImage * image = GTK_IMAGE(gtk_builder_get_object(builder,"ImageToPrint"));
+    GtkButton * Nextbutton = GTK_BUTTON(gtk_builder_get_object(builder,"NextButton"));
+    GtkLevelBar * ProgressBar = GTK_LEVEL_BAR(gtk_builder_get_object(builder,"ProgressBar"));
+    GtkLabel * ProcessLabel = GTK_LABEL(gtk_builder_get_object(builder,"ProcessLabel"));
+
     infos.image = image;
-    printf("%p\n",image);
-    printf("%p\n",file_chooser);
 
     // Connects signal handlers.
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
     g_signal_connect(file_chooser, "file-set", G_CALLBACK(create_image), &infos);
-
+    
     // Runs the main loop.
     gtk_main();
 
