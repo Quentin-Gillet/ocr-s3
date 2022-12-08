0a4be32e4d57c2e1e1e2481191902d2c8754beab
Alexis COGNET <alexis.cognet@epita.fr>
2022-12-08 21:29:37 +0100

    Starting to config the Next button

--- interfacegtk.c
+++ interfacegtk.c
@@ -2,6 +2,10 @@
 
 typedef struct appinfo {
     GtkImage* image;
+    int CurrEvent;
+    GtkButton * Nextbutton;
+    GtkLevelBar * ProgressBar;
+    GtkLabel * ProcessLabel;
 } AppInfo;
 
 void create_image(GtkFileChooserButton* file_chooser_button, gpointer user_data)
@@ -13,7 +17,27 @@ void create_image(GtkFileChooserButton* file_chooser_button, gpointer user_data)
     const GdkPixbuf * pixbuf = gdk_pixbuf_new_from_file(filename,NULL);
     GdkPixbuf * ResizedPixbuf = gdk_pixbuf_scale_simple(pixbuf,1000,850,GDK_INTERP_BILINEAR);
     gtk_image_set_from_pixbuf(info->image,ResizedPixbuf); // filename est un const char*
-    
+    gtk_widget_set_sensitive(GTK_WIDGET(info->Nextbutton),TRUE); 
+}
+
+void next_event(GtkButton * Nextbutton, gpointer user_data)
+{
+    AppInfo * info = user_data;
+    info->CurrEvent = info->CurrEvent + 1;
+    printf("currevent : %i\n",info->CurrEvent);
+    gtk_button_set_label(Nextbutton, "Next Step");
+    double BarValue = gtk_level_bar_get_value(info->ProgressBar);
+    // to set the value of the bar : gtk_level_bar_set_value(info->ProgressBar,BarValue+10);
+    GtkLabel * ProcessLabel = info->ProcessLabel;
+    switch(info->CurrEvent)
+    {
+	    case 1:
+		    gtk_label_set_label(ProcessLabel,"TaMère qui bois du sprite sa mère");
+		    break;
+	    default:
+		    break;
+    }
+
 }
 
 // Main function.
@@ -44,11 +68,15 @@ int main (int argc, char *argv[])
     GtkLabel * ProcessLabel = GTK_LABEL(gtk_builder_get_object(builder,"ProcessLabel"));
 
     infos.image = image;
+    infos.CurrEvent = -1;
+    infos.Nextbutton = Nextbutton;
+    infos.ProgressBar = ProgressBar;
+    infos.ProcessLabel = ProcessLabel;    
 
     // Connects signal handlers.
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
     g_signal_connect(file_chooser, "file-set", G_CALLBACK(create_image), &infos);
-    
+    g_signal_connect(Nextbutton, "clicked", G_CALLBACK(next_event),&infos);
     // Runs the main loop.
     gtk_main();
 
